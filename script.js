bff2f4d10f4d3122162fc249012ec7ec0601405f
Alexisfc91011 <jonathanmacasperez2019@gmail.com>
2025-06-21 18:56:22 -0500

    Estructura básica: HTML, CSS, pala dibujada

---
+++ script.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+let paddleY = 150;
+const paddleHeight = 100;
+const paddleWidth = 10;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, paddleY, paddleWidth, paddleHeight);
+}
+
+setInterval(draw, 1000 / 60);
