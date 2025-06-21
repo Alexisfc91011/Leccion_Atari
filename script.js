162757d9f64f514182281e092c7dcd8810494ee4
Alexisfc91011 <jonathanmacasperez2019@gmail.com>
2025-06-21 18:58:13 -0500

    Agregado movimiento de la pala con el mouse

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function draw() {
 }
 
 setInterval(draw, 1000 / 60);
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+let paddleY = 150;
+const paddleHeight = 100;
+const paddleWidth = 10;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, paddleY, paddleWidth, paddleHeight);
+}
+
+canvas.addEventListener("mousemove", function(event) {
+  const mouseY = event.clientY - canvas.getBoundingClientRect().top;
+  paddleY = mouseY - paddleHeight / 2;
+});
+
+setInterval(draw, 1000 / 60);
